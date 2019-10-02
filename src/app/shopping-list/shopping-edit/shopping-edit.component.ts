b7bab00b1ca67bfd4fadb32943f21737c8f83f29
Liam Swift <liam.swift@rhq-mb-ksa-12.wa.local>
2019-10-02 15:58:11 +1000

    ingredients can now be populated

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', { static: false } ) nameInput;
+  @ViewChild('amountInput', { static: false } ) amountInput;
+
+  @Output() ingredients: EventEmitter<Ingredient> = new EventEmitter;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitForm() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+    this.ingredients.emit(new Ingredient(name, amount));
+
+    this.nameInput.nativeElement.value = null;
+    this.amountInput.nativeElement.value = null;
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -20,4 +20,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  addIngredientsToList(inputIngredients: Ingredient) {
+    this.ingredients.push(inputIngredients)
+  }
+
 }
